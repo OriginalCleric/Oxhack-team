9c0b1670110632c4dd693637dd528591f3a6485a
Dmitry Filippov <dmitriyvfilippov@gmail.com>
2022-02-26 20:56:09 +0000

    some classes for the graph

---
+++ Back-End/state.py
@@ -0,0 +1,26 @@
+
+
+class WaterFascility:
+    maxWater = 0
+    water = 0
+
+class Energy:
+    energyLevel = 0
+
+class Food:
+    food = 0
+
+class Population:
+    people = 0
+
+class Fuel:
+    uranium = 0
+    tharium = 0
+
+class ConstructionMaterials:
+    materials = 0
+
+class Reactor:
+    
+
+class State:    
